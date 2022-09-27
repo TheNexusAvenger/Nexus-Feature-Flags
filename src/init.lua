5616ad4e66d0053cd9b4ddcc5ef1481f5a1a00f9
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-09-26 22:03:29 -0400

    Ensure singleton instance if multiple modules used (like Nexus Admin).

--- src/init.lua
+++ src/init.lua
@@ -4,12 +4,16 @@ TheNexusAvenger
 Main module for Nexus Feature Flags.
 --]]
 
-if game:GetService("RunService"):IsClient() then
-    return require(script:WaitForChild("Source"):WaitForChild("StringValueSource")).new(script:WaitForChild("FeatureFlags"))
-else
-    local FeatureFlagsValue = Instance.new("StringValue")
-    FeatureFlagsValue.Name = "FeatureFlags"
-    FeatureFlagsValue.Parent = script
+if not _G.NexusFeatureFlagsSingletonInstance then
+    if game:GetService("RunService"):IsClient() then
+        _G.NexusFeatureFlagsSingletonInstance = require(script:WaitForChild("Source"):WaitForChild("StringValueSource")).new(script:WaitForChild("FeatureFlags"))
+    else
+        local FeatureFlagsValue = Instance.new("StringValue")
+        FeatureFlagsValue.Name = "FeatureFlags"
+        FeatureFlagsValue.Parent = script
 
-    return require(script:WaitForChild("Source"):WaitForChild("NexusDataStoreSource")).new(FeatureFlagsValue)
+        _G.NexusFeatureFlagsSingletonInstance = require(script:WaitForChild("Source"):WaitForChild("NexusDataStoreSource")).new(FeatureFlagsValue)
+    end
 end
+
+return _G.NexusFeatureFlagsSingletonInstance
